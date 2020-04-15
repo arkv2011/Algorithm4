68b081f5c4972c42badd6f1b7b4d200326b739f0
Arkv <shakar@live.cn>
2020-04-15 22:47:26 +0800

    The first step

---
+++ Algorithm4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algs4Lib;
+
+namespace Algorithm4
+{
+    class Program
+    {
+
+        static void RandomTest()
+        {
+            int count = 0;
+            for(int i=0;i<10000;i++)
+            {
+                if (StdRandom.bernoulli())
+                    ++count;
+            }
+            Console.WriteLine($"True:{count}\nFalse:{10000 - count}");
+        }
+
+        static void Main(string[] args)
+        {
+            RandomTest();
+        }
+    }
+}
---
+++ Algs4Lib/StdRandom.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algs4Lib
+{
+    public class StdRandom
+    {
+        /// <summary>
+        /// Seed of the random generator.
+        /// </summary>
+        static long seed = DateTime.Now.Millisecond;
+        static Random rdm = new Random();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static long Seed
+        {
+            get { return seed; }
+            set
+            {
+                seed = value;
+                rdm = new Random(Convert.ToInt32(seed));
+            }
+
+        }
+
+        public static double uniform()
+        {
+            return rdm.NextDouble();
+        }
+
+        public static int uniform(int N)
+        {
+            if (N <= 0)
+                throw new ArgumentOutOfRangeException("N", "Argument must be positive");
+            return rdm.Next(N);
+        }
+
+        public static double uniform(int a, int b)
+        {
+            if (b >= a)
+                throw new ArgumentException($"Invalid range:[{a}, {b})");
+            return a + uniform() * (b - a);
+        }
+
+        public static bool bernoulli(double p)
+        {
+            if (p > 1 || p < 0)
+                throw new ArgumentOutOfRangeException("p", "p must be between 0.0 and 1.0");
+            return uniform() < p;
+        }
+
+        public static bool bernoulli()
+        {
+            return bernoulli(0.5);
+        }
+    }
+}
